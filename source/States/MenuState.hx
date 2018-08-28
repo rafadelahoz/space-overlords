8790356da1f0f0e7042c6a5be510b8d082c6a89a
Rafa de la Hoz <rafa.delahoz@gmail.com>
2018-08-28 19:09:49 +0200

    Updated cell feed screen to match style

--- source/States/MenuState.hx
+++ source/States/MenuState.hx
@@ -23,7 +23,11 @@ class MenuState extends GarbageState
     var background : FlxSprite;
     var backgroundShader : FlxSprite;
 
-    var startTouchZone : FlxObject;
+    var backButton : VcrButton;
+    var playButton : VcrButton;
+    var museumButton : VcrButton;
+    var rewardButton : VcrButton;
+    var cursor : FlxSprite;
 
     var interactable : Bool;
 
@@ -34,8 +38,6 @@ class MenuState extends GarbageState
     override public function create():Void
     {
         super.create();
-
-        // Missing a preloader
 
         interactable = false;
 
@@ -48,60 +50,48 @@ class MenuState extends GarbageState
         background = new FlxSprite(0, 0, "assets/backgrounds/bgCell.png");
         add(background);
 
-        var slaveNumberBg : FlxSprite = new FlxSprite(57, 33).makeGraphic(117, 14, 0xFF9e2835);
-        add(slaveNumberBg);
+        // Generic header
+        add(new FlxSprite(0, 0, "assets/ui/title-menu-header.png"));
 
-        var logo : FlxSprite = new FlxSprite(0, 0, "assets/ui/title.png");
+        var baseY : Float = 36;
+        
+        var logo : FlxSprite = new FlxSprite(0, baseY, "assets/ui/cell-menu-main.png");
         add(logo);
 
         var footer : FlxSprite = new FlxSprite(0, 276, "assets/ui/title-menu-footer.png");
         add(footer);
         add(new VcrClock());
 
-        var slaveNumber : FlxBitmapText = text.VcrText.New(111, 34, text.TextUtils.padWith("" + FlxG.random.int(1, 9999999), 7, "0"));
+        var slaveNumber : FlxBitmapText = text.VcrText.New(107, baseY+24, text.TextUtils.padWith("" + FlxG.random.int(1, 9999999), 7, "0"));
         add(slaveNumber);
 
-        var backButton : VcrButton = new VcrButton(8, logo.height + 12, null, onBackPressed);
+        backButton = new VcrButton(8, 83, onBackHighlighted, onBackPressed);
         backButton.loadSpritesheet("assets/ui/title-menu-back.png", 56, 14);
         add(backButton);
 
-        startLabelBackground = new FlxSprite(27, 88+32).makeGraphic(141, 16, 0xFF9e2835);
-        startLabelBackground.visible = false;
-        add(startLabelBackground);
+        playButton = new VcrButton(26, 119, onPlayHighlighted, onArcadeButtonPressed);
+        playButton.loadSpritesheet("assets/ui/cell-menu-newgame.png", 137, 14);
+        add(playButton);
 
-        touchLabel = new FlxSprite(0, 72+32, "assets/ui/tmp-start-label.png");
-        touchLabel.alpha = 0;
-        add(touchLabel);
+        museumButton = new VcrButton(26, 143, onMuseumHighlighted, onMuseumPressed);
+        museumButton.loadSpritesheet("assets/ui/cell-menu-museum.png", 137, 14);
+        add(museumButton);
+
+        cursor = new FlxSprite(9, playButton.y+1, "assets/ui/title-menu-cursor.png");
+        add(cursor);
 
         // VCR effect
         add(new Scanlines(0, 0, "assets/ui/vcr-overlay.png"));
 
-        var startDelay : Float = 0.35;
-        tween = FlxTween.tween(logo, {y : 0}, 0.75, {startDelay: startDelay, onComplete: onLogoPositioned, ease : FlxEase.quartOut });
+        // var startDelay : Float = 0.35;
+        // tween = FlxTween.tween(logo, {y : 0}, 0.75, {startDelay: startDelay, onComplete: onLogoPositioned, ease : FlxEase.quartOut });
+        onLogoPositioned();
         FlxG.camera.scroll.set(0, 0);
     }
 
-    public function onLogoPositioned(_t:FlxTween):Void
+    public function onLogoPositioned(?_t:FlxTween = null):Void
     {
         interactable = true;
-
-        startTouchZone = new FlxObject(0, 72+32, Constants.Width, 32);
-        add(startTouchZone);
-
-        FlxTween.tween(touchLabel, {alpha : 1}, 0.2, {ease : FlxEase.bounceInOut});
-        // startTouchBuzz(null);
-    }
-
-    function startTouchBuzz(_t:FlxTween)
-    {
-        var touchLabelBaseY = touchLabel.y;
-        FlxTween.tween(touchLabel, {y : touchLabelBaseY-4}, 0.2, {ease: FlxEase.circOut, startDelay: 2, onComplete: continueTouchBuzz});
-    }
-
-    function continueTouchBuzz(_t:FlxTween)
-    {
-        var touchLabelBaseY = touchLabel.y;
-        FlxTween.tween(touchLabel, {y : touchLabelBaseY+4}, 0.5, {ease: FlxEase.elasticOut, onComplete: startTouchBuzz});
     }
 
     override public function destroy():Void
@@ -109,65 +99,26 @@ class MenuState extends GarbageState
         super.destroy();
     }
 
-    override public function update(elapsed:Float):Void
+    function onBackHighlighted()
     {
-        if (FlxG.keys.justPressed.O)
-            Screenshot.take();
-
-        if (interactable)
-        {
-            #if (!mobile)
-            if (startTouchZone.getHitbox().containsPoint(FlxG.mouse.getPosition()))
-            {
-                if (FlxG.mouse.pressed)
-                {
-                    onTouchLabelPressed();
-                }
-                else if (FlxG.mouse.justReleased)
-                {
-                    onTouchLabelReleased();
-                    onArcadeButtonPressed();
-                }
-            }
-            #else
-            for (touch in FlxG.touches.list)
-            {
-                if (touch.overlaps(startTouchZone))
-                {
-                    if (touch.pressed)
-                    {
-                        onTouchLabelPressed();
-                    }
-                    else if (touch.justReleased)
-                    {
-                        onTouchLabelReleased();
-                        onArcadeButtonPressed();
-                        break ;
-                    }
-                }
-                else if (touchLabel.color != 0xFFFFFFFF)
-                {
-                    if (touch.justReleased)
-                    {
-                        onTouchLabelReleased();
-                        break ;
-                    }
-                }
-            }
-            #end
-        }
-
-        super.update(elapsed);
+        playButton.clearHighlight();
+        museumButton.clearHighlight();
     }
 
-    function onTouchLabelPressed()
+    function onPlayHighlighted()
     {
-        startLabelBackground.visible = true;
+        backButton.clearHighlight();
+        museumButton.clearHighlight();
+
+        cursor.y = playButton.y+1;
     }
 
-    function onTouchLabelReleased()
+    function onMuseumHighlighted()
     {
-        startLabelBackground.visible = false;
+        backButton.clearHighlight();
+        playButton.clearHighlight();
+
+        cursor.y = museumButton.y+1;
     }
 
     public function onArcadeButtonPressed() : Void
@@ -175,6 +126,11 @@ class MenuState extends GarbageState
         FlxG.camera.fade(0xFF000000, 0.5, false, function() {
             GameController.StartEndless();
         });
+    }
+
+    function onMuseumPressed()
+    {
+        // Nop!
     }
 
     public function onBackPressed()
